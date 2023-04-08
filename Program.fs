ecfbc85fccd3f8b2daa0740c3b05b60eb521bfb1
Aarón Carrizal <aaronk4572@gmail.com>
2023-04-08 15:04:03 -0600

    data can be entered

--- Program.fs
+++ Program.fs
@@ -1,1 +1,25 @@
 open Busqueda
+open System
+
+let parseStringToIntList (input: string) : int list =
+    input.Split([|' '|], StringSplitOptions.RemoveEmptyEntries)
+    |> Array.toList
+    |> List.map Int32.Parse
+
+let readMultipleLines() =
+    let rec readLines acc =
+        let line = Console.ReadLine()
+        if String.IsNullOrWhiteSpace(line) then
+            List.rev acc
+        else
+            let line2 = parseStringToIntList line
+            readLines (line2::acc)
+    readLines []
+
+let main() =
+    printfn "Enter multiple lines of text (press Enter on an empty line to finish):"
+    let lines = readMultipleLines()
+    printf "%A" lines
+
+main()
+
