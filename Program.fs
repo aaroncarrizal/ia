d15e0d8141c436f98baf09eb20a2d7ebdda2bada
Aarón Carrizal <aaronk4572@gmail.com>
2023-04-11 13:47:06 -0600

    backtracking funciona al 100

--- Program.fs
+++ Program.fs
@@ -1,5 +1,6 @@
 open Busqueda
 open System
+open System.Diagnostics
 
 let parseStringToIntList (input: string) : int list =
     input.Split([|' '|], StringSplitOptions.RemoveEmptyEntries)
@@ -24,6 +25,9 @@ let convertListToArray (input: int list list) : int[][] =
 
 let main() =
     // Función para verificar si un número es válido en una casilla del sudoku
+    (*
+        FUNCIÓN META
+    *)
     let rec isValid (board:int[][]) row col num =
         let mutable valid = true
 
@@ -49,13 +53,17 @@ let main() =
         valid
 
     // Función para resolver el sudoku utilizando backtracking
-    let rec solveSudoku (board:int[][]) row col =
+    (*
+        FUNCIÓN SUCESORA
+     *)
+    let rec solveSudoku (board:int[][]) row col (count: int ref) =
+        count := count.Value + 1
         if row = 9 then
             true // Si se llega al final del tablero, está resuelto
         else if col = 9 then
-            solveSudoku board (row + 1) 0 // Si se llega al final de una fila, pasar a la siguiente fila
+            solveSudoku board (row + 1) 0 count// Si se llega al final de una fila, pasar a la siguiente fila
         else if board.[row].[col] <> 0 then
-            solveSudoku board row (col + 1) // Si la casilla ya tiene un número, pasar a la siguiente casilla
+            solveSudoku board row (col + 1) count// Si la casilla ya tiene un número, pasar a la siguiente casilla
         else
             // Casilla vacía encontrada, probar números del 1 al 9
             let mutable result = false // Se utiliza mutable para almacenar el resultado de la recursión
@@ -65,7 +73,7 @@ let main() =
                     board.[row].[col] <- num
 
                     // Se llama recursivamente a la función para seguir probando
-                    if solveSudoku board row (col + 1) then
+                    if solveSudoku board row (col + 1) count then
                         // Si se resuelve el sudoku, se actualiza el resultado y se sale del bucle
                         result <- true
                     else
@@ -74,11 +82,6 @@ let main() =
 
             result // Se retorna el resultado de la recursión al final de la función
 
-
-    printfn "Dame el sudoku inicial:"
-    let lines = readMultipleLines()
-    let sudokuBoard = convertListToArray lines
-
     // Función para imprimir el tablero del sudoku en la consola
     let printBoard (board:int[][]) =
         for row in board do
@@ -86,12 +89,27 @@ let main() =
                 printf "%i " col
             printfn ""
 
+    let stopwatch = Stopwatch()
+    printfn "Dame el sudoku inicial:"
+    // Leer la entrada del usuario
+    let lines = readMultipleLines()
+    let sudokuBoard = convertListToArray lines
+    // Empezar a contar el tiempo de ejecución
+    stopwatch.Start()
+    // Declarar contador de nodos visitados
+    let mutable nodes = 0
+    let nodesRef = ref nodes
     // Resolver el sudoku
-    if solveSudoku sudokuBoard 0 0 then
+    if solveSudoku sudokuBoard 0 0 nodesRef then
         printfn "Sudoku resuelto:"
         printBoard sudokuBoard
     else
         printfn "No se pudo resolver el sudoku."
+    stopwatch.Stop()
+    let elapsedTimeMs = stopwatch.ElapsedMilliseconds
+    printfn "Nodos: %i" nodesRef.Value
+    printfn "Tiempo: %d ms" elapsedTimeMs
+    printfn "Factor de ramificación promedio: %f" (float nodesRef.Value / float 81)
 
 main()
 
