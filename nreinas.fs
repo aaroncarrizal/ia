22ef7748cedc905ecf9d6f19a0efc595a20e4bd1
Aarón Mishael Carrizal Méndez <180816@upslp.edu.mx>
2023-04-11 15:52:04 -0600

    aaaa

--- nreinas.fs
+++ nreinas.fs
@@ -9,5 +9,42 @@ module nreinas =
         [1..n]
         |> List.map(fun _ -> rnd.Next(n))
 
-let sucesores estado = 
-    
+    let sucesores estado = 
+        let mover columna = 
+            List.mapi( fun i _ ->
+                List.mapi( fun j x ->
+                    if j <> columna
+                    then x
+                    else i) estado) estado
+            |> List.filter( fun estado' -> estado' <> estado)
+        estado
+        |> List.mapi(fun columna _ -> mover columna)
+        |> List.collect id
+        |> List.map( fun estado -> ((), estado))
+
+    let atacan ((x1: int, y1: int), (x2: int, y2: int)) =
+        if y1 = y2 || Math.Abs(x2 - x1) = Math.Abs (y2 - y1) then
+            1 
+        else 0
+
+    //funcion heuristica
+    let paresAtacando estado = 
+        let reinas = 
+            estado
+            |> List.mapi(fun col fila -> (col, fila))
+        reinas
+        |> List.allPairs reinas
+        |> List.filter( fun (r1, r2) -> r1 <> r2)
+        |> List.sumBy atacan
+        |> (fun n -> n/2)
+
+    let meta estado = 
+        paresAtacando estado = 0 
+
+    let problema n = 
+        {
+            inicio = inicio n
+            sucesores = sucesores
+            meta = meta
+            costo = fun _ _ _ -> 1.0
+        }
